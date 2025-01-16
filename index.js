959dd14ab66cba64ec22e96dc08ab39673ee8a03
Carlos García <carlossalgt@gmail.com>
2025-01-16 10:54:15 -0600

    Adding readable file and convert to json log

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
+const csv = require("fast-csv");
+const os = require("os");
+const fs = require("fs");
 const app = express();
+const fileUpload = require("express-fileupload");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: os.tmpdir(),
+  })
+);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/upload.html"));
 });
+
+app.post("/upload", (req, res) => {
+  let files = req.files || null;
+  let type = req.body["type"] || null;
+
+  console.log({ files, type });
+
+  if (!files || Object.keys(files).length === 0 || !type) {
+    res.redirect(`/?status=error&code=1001`);
+    return;
+  }
+
+  let file = files.csv;
+  convertFile(file)
+    .then((response) => {
+      let url = "/?status=success";
+      if (!response.success) {
+        let responseErrorMessage = response.message
+          ? encodeURIComponent(response.message)
+          : "";
+        url = `/?status=error&code=${0}&message=${responseErrorMessage}`;
+      }
+      res.redirect(url);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+const convertFile = (file) => {
+  return new Promise((resolve, reject) => {
+    let models = [];
+    fs.createReadStream(file.tempFilePath)
+      .pipe(csv.parse({ headers: true }))
+      .on("error", (err) => {
+        reject(err);
+      })
+      .on("data", (row) => {
+        console.log({ current_row: row });
+      })
+      .on("end", async (rowCount) => {
+        console.log({ rowCount });
+        return resolve({ success: true });
+      });
+  });
+};
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
